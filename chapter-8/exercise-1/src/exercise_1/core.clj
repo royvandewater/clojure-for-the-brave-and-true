b4a423595831c2a15815644298658f75432f1800
Roy van de Water <roy.v.water@gmail.com>
2018-05-15 13:46:14 -0700

    It works!

--- chapter-8/exercise-1/src/exercise_1/core.clj
+++ chapter-8/exercise-1/src/exercise_1/core.clj
@@ -1,1 +1,6 @@
 (ns exercise-1.core)
+
+(defmacro when-valid
+  "Executes the function if the thing passes the condition"
+  [thing condition f]
+  `(if (~condition ~thing) (~f)))
--- chapter-8/exercise-1/test/exercise_1/core_test.clj
+++ chapter-8/exercise-1/test/exercise_1/core_test.clj
@@ -1,6 +1,15 @@
 (ns exercise-1.core-test
   (:require [clojure.test :refer :all]
-            [exercise-1.core :refer :all]))
+            [exercise-1.core :refer :all]
+            [exercise-1.spy :refer [create-spy called?]]))
 
-(deftest a-test1
-  (testing "Basic tests"))
+(deftest when-valid-test
+  (testing "When called with a valid thing"
+    (let [spy (create-spy)]
+      (when-valid 0 #(= % 0) spy)
+      (is (called? spy))))
+
+  (testing "When called with an invalid thing"
+    (let [spy (create-spy)]
+      (when-valid 1 #(= % 0) spy)
+      (is (not (called? spy))))))
