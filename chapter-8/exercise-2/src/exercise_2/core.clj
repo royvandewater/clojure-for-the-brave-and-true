734e99a2d681948218550787a6662d7cc3414240
Roy van de Water <roy.v.water@gmail.com>
2018-05-16 09:02:27 -0700

    Add implementation of or as a macro

--- chapter-8/exercise-2/src/exercise_2/core.clj
+++ chapter-8/exercise-2/src/exercise_2/core.clj
@@ -1,6 +1,12 @@
 (ns exercise-2.core)
 
-(defn foo
+(defmacro macro_or
   "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+  ([] false)
+
+  ([x] x)
+
+  ([x & next]
+   `(let [val# ~x]
+     (if val# val# (macro_or ~@next)))))
--- chapter-8/exercise-2/test/exercise_2/core_test.clj
+++ chapter-8/exercise-2/test/exercise_2/core_test.clj
@@ -1,7 +1,30 @@
 (ns exercise-2.core-test
   (:require [clojure.test :refer :all]
-            [exercise-2.core :refer :all]))
+            [exercise-2.core :refer :all]
+            [basic_spy.core :refer [create-spy call-count called?]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest or-test
+  (testing "When given no parameters should return false"
+    (is (false? (macro_or))))
+
+  (testing "When given true should return true"
+    (is (true? (macro_or true))))
+
+  (testing "When given false should return false"
+    (is (false? (macro_or false))))
+
+  (testing "When given false and true should return true"
+    (is (true? (macro_or false true))))
+
+  (testing "When the first argument is true, the second should not be evaluated"
+    (let [spy (create-spy)]
+      (macro_or (constantly true) (spy))
+      (is (not (called? spy)))))
+
+  (testing "When given a truthy value and false should return the truthy value"
+    (is (= 1 (macro_or 1 nil))))
+
+  (testing "When given a function that returns a truthy value, should only evaluate once"
+    (let [spy (create-spy (constantly true))]
+      (macro_or (spy) nil)
+      (is (= 1 (call-count spy))))))
