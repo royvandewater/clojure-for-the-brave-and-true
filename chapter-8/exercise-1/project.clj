5be32662a2cca6192709fb273b88f6c799c3f1c3
Roy van de Water <roy.vwater@gmail.com>
2018-10-22 14:58:54 -0700

    Manually rollbar for rollcage dev

--- chapter-8/exercise-1/project.clj
+++ chapter-8/exercise-1/project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [basic-spy "1.0.0"]
-                 [proto-repl "0.3.1"]]
+                 [proto-repl "0.3.1"]
+                 [circleci/rollcage "1.0.0-SNAPSHOT"]]
   :main ^:skip-aot exercise-1.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
--- chapter-8/exercise-1/test/exercise_1/core_test.clj
+++ chapter-8/exercise-1/test/exercise_1/core_test.clj
@@ -1,7 +1,8 @@
 (ns exercise-1.core-test
   (:require [clojure.test :refer :all]
             [exercise-1.core :refer :all]
-            [basic_spy.core :refer [create-spy called?]]))
+            [basic_spy.core :refer [create-spy called?]]
+            [circleci.rollcage.core :as rollcage]))
 
 (deftest when-valid-test
   (testing "When called with a valid thing"
@@ -21,3 +22,13 @@
     (let [spy (create-spy)]
       (when-valid 1 #(= % 0) (spy))
       (is (not (called? spy))))))
+
+
+(deftest rollbar-test
+  (testing "Creating a rollbar"
+    (let [token  "4355d12c64094ce5be54ef58bac739b4"
+          client (rollcage/client token {:environment "testing"})]
+      (try
+        (/ 0)
+        (catch Exception e
+          (rollcage/error client e))))))
